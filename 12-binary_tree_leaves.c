df980283ad8d9fd7f1fbdfa1c363a7597c1acea8
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-04 16:28:24 +0300

    Code update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,14 +8,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t l = 0;
 
 	if (tree)
 	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
+		l += (!tree->left && !tree->right) ? 1 : 0;
+
+		l += binary_tree_leaves(tree->left);
+
+		l += binary_tree_leaves(tree->right);
 	}
-	return (leaves);
+	return (l);
 }
 
